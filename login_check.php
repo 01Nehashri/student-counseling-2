790038fe016c7ebfa0b974b898cb07aa9e10aa41
01Nehashri <107236969+01Nehashri@users.noreply.github.com>
2022-08-28 05:45:35 +0530

    Add files via upload

--- login_check.php
+++ login_check.php
@@ -1,75 +1,50 @@
 <?php 
-session_start();
+if ($_POST['submit'] == 'Login'){ 
+//Collect POST values 
+$login = $_POST['login']; 
+$password = $_POST['password']; 
+if($login && $password){ 
+//Connect to mysql server 
+$link = @mysql_connect('dbms.iiitdmj.ac.in', '2016162', '292795f5'); 
+//Check link to the mysql server 
+if(!$link) { 
+die('Failed to connect to server: ' . mysql_error()); 
+} 
+//Select database 
+$db = mysql_select_db('2016162'); 
+if(!$db) { 
+die("Unable to select database"); 
+} 
+//Create query (if you have a Logins table the you can select login id and password from there)
+//$qry='SELECT * FROM Logins WHERE login_id = "ABC" AND password = "12345"'; 
+//Execute query 
+//$result=mysql_query($qry); 
+//Check whether the query was successful or not 
+$qry= "SELECT password FROM counsel_member WHERE cons_id=$login"	;
+$query = mysql_fetch_array(mysql_query($qry));
 
-if($_POST['Ssubmit']) {
-	$email=$_POST['Semail'];
-	$password = $_POST['Spassword']; 
-	if($password && $email){ 
-	//Connect to mysqli server 
-	$link = mysqli_connect('dbms.iiitdmj.ac.in','2016162','292795f5'); 
-	//Check link to the mysqli server 
-	if(!$link) { 
-	die('Failed to connect to server: ' . mysqli_error()); 
-	} 
-	//Select database 
-	$db = mysqli_select_db($link,'2016162'); 
-	if(!$db) { 
-	die("Unable to select database"); 
-	} 
-	//Create query (if you have a Logins table the you can select login id and password from there)
-	$qry= "SELECT password FROM student WHERE email='$email'";
-	$result=mysqli_query($link,$qry) or die(mysqli_error($link));
-	$array=mysqli_fetch_array($result);
-	if($password == $array['password']) { 
-		$_SESSION["user_id"] = $email; 
-		header('Location:student_profile.php?user_id='.$email);
-	}
-	else{ 
-		//Login failed 
-		header('Location: login.php?msg=serror');
-		exit(); 
-	} 
-	}
-
-	else{ 
-	include('login.php'); 
-	echo '<center>Username or Password missing!!</center>'; 
-	exit(); 
-	} 
+if($password == $query['password']) {
+	include('counseler_login.html');
+	echo'<center> Correct Username and Password !!!</center>';
+	$count=1;
 }
-else {
-	$email=$_POST['Cemail'];
-	$password = $_POST['Cpassword']; 
-	if($password && $email){ 
-	//Connect to mysqli server 
-	$link = mysqli_connect('dbms.iiitdmj.ac.in','2016162','292795f5'); 
-	//Check link to the mysqli server 
-	if(!$link) { 
-	die('Failed to connect to server: ' . mysqli_error()); 
-	} 
-	//Select database 
-	$db = mysqli_select_db($link,'2016162'); 
-	if(!$db) { 
-	die("Unable to select database"); 
-	} 
-	//Create query (if you have a Logins table the you can select login id and password from there)
-	$qry= "SELECT password FROM counsel_member WHERE email='$email'";
-	$result=mysqli_query($link,$qry) or die(mysqli_error($link));
-	$array=mysqli_fetch_array($result);
-	if($password == $array['password']) {
-		header('Location:counsel.php');
-	}
-	else{ 
-		//Login failed 
-		header('Location: login.php?msg=cerror');
-		exit(); 
-	} 
-	}
-
-	else{ 
-	include('login.php'); 
-	echo '<center>Username or Password missing!!</center>'; 
-	exit(); 
-	} 
+else{ 
+//Login failed 
+include('counseler_login.html'); 
+echo '<center>Incorrect Username or Password !!!</center>'; 
+exit(); 
+} 
 }
+
+
+else{ 
+include('counseler_login.html'); 
+echo '<center>Username or Password missing!!</center>'; 
+exit(); 
+} 
+} 
+else{ 
+header("location: counseler_login.html"); 
+exit(); 
+} 
 ?>
